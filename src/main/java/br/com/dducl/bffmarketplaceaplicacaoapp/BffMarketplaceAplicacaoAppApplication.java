444e3cfe919bbea5a36371a0e4c50c1252ab2437
Danilo Faccio <facciodrums@gmail.com>
2023-10-04 22:04:05 -0300

    primeiro commit, criando repositorio com spring

---
+++ src/main/java/br/com/dducl/bffmarketplaceaplicacaoapp/BffMarketplaceAplicacaoAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.dducl.bffmarketplaceaplicacaoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffMarketplaceAplicacaoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffMarketplaceAplicacaoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dducl/bffmarketplaceaplicacaoapp/BffMarketplaceAplicacaoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dducl.bffmarketplaceaplicacaoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffMarketplaceAplicacaoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

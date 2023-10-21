4c3cdc85659b2254a2c1b1d2bdad44ef69de6b94
DFaccio <39106747+DFaccio@users.noreply.github.com>
2023-10-21 13:01:13 -0300

    adicionando tratamento de excecoes para uma mensagem mais amigavel (#17)

---
+++ src/main/java/br/com/dducl/bffmarketplaceapp/util/StandardError.java
@@ -0,0 +1,55 @@
+package br.com.dducl.bffmarketplaceapp.util;
+
+import java.time.Instant;
+
+public class StandardError {
+
+    private Instant timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+    public StandardError(){
+
+    }
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}

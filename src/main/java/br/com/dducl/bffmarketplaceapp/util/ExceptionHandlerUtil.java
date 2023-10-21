7f2d5817f5a02333fec070aac3919e2ee8c3f710
Danilo Faccio <facciodrums@gmail.com>
2023-10-21 12:54:06 -0300

    adicionando tratamento de excecoes para uma mensagem mais amigavel

---
+++ src/main/java/br/com/dducl/bffmarketplaceapp/util/ExceptionHandlerUtil.java
@@ -0,0 +1,28 @@
+package br.com.dducl.bffmarketplaceapp.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ExceptionHandlerUtil {
+
+    private StandardError err = new StandardError();
+
+    @ExceptionHandler(ValidacoesException.class)
+    public ResponseEntity<StandardError> entityNotFound(ValidacoesException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Entity not found");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(this.err);
+
+    }
+}

4fb8eb25940aa15327a0e9790b1744defbb19d6f
DFaccio <39106747+DFaccio@users.noreply.github.com>
2023-10-26 21:46:01 -0300

    Atualizando a main com a classe para tratamento de Produtos
    Criação do classes para tratamento de produtos

---
+++ src/main/java/br/com/dducl/bffmarketplaceapp/controller/ProdutoController.java
@@ -0,0 +1,54 @@
+package br.com.dducl.bffmarketplaceapp.controller;
+
+import br.com.dducl.bffmarketplaceapp.dto.ProdutoDto;
+import br.com.dducl.bffmarketplaceapp.negocio.ProdutoBusiness;
+import br.com.dducl.bffmarketplaceapp.util.Pagination;
+import br.com.dducl.bffmarketplaceapp.util.ResultadoPaginado;
+import br.com.dducl.bffmarketplaceapp.util.ValidacoesException;
+import jakarta.annotation.Resource;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/produto")
+public class ProdutoController {
+
+    @Resource
+    private ProdutoBusiness business;
+
+    @GetMapping
+    public ResponseEntity<ResultadoPaginado<ProdutoDto>> findAll(@RequestParam(required = false) Integer pageSize,
+                                                                @RequestParam(required = false) Integer initialPage) {
+
+        Pagination page = new Pagination(initialPage, pageSize);
+
+        return ResponseEntity.ok(business.findAll(page));
+    }
+
+    @GetMapping(value = "/id/{id}")
+    public ResponseEntity<ProdutoDto> findProdutoById(@PathVariable int id){
+        return ResponseEntity.ok(business.findProdutoById(id));
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<ProdutoDto> insert(@RequestBody ProdutoDto produto) throws ValidacoesException {
+        produto = business.insert(produto);
+
+        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(produto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ProdutoDto> update(@RequestBody ProdutoDto produto) throws ValidacoesException {
+        produto = business.update(produto);
+
+        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(produto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ProdutoDto> delete(@PathVariable int id){
+        ProdutoDto produto = business.delete(business.findProdutoById(id));
+
+        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(produto);
+    }
+}

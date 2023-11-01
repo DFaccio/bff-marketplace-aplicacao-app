1752aac62bb64992d6efe0eccb3ed8b3525094fb
Isabela França <fysabelah@gmail.com>
2023-10-31 22:40:02 -0300

    Corrigir validação no dto de cadastro de usuário.

--- src/main/java/br/com/dducl/bffmarketplaceapp/dto/UsuarioCadastroDto.java
+++ src/main/java/br/com/dducl/bffmarketplaceapp/dto/UsuarioCadastroDto.java
@@ -1,7 +1,6 @@
 package br.com.dducl.bffmarketplaceapp.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +13,6 @@ public class UsuarioCadastroDto extends UsuarioDto {
     @NotBlank(message = "Senha é um campo obrigatório")
     private String password;
 
-    @PositiveOrZero(message = "O identificador da pessoa é um campo obrigatório")
+    @NotBlank(message = "O identificador da pessoa é um campo obrigatório")
     private String pessoaId;
 }

5a83e04f8c3d9991ec73fce4db9159a4a601161e
Isabela França <fysabelah@gmail.com>
2024-09-01 12:39:18 -0300

    Refatoração em portfólio validação dos metódos

--- src/main/java/br/com/dducl/collective_coffee_marketplace/util/exceptions/NotFoundException.java
+++ src/main/java/br/com/dducl/collective_coffee_marketplace/util/exceptions/NotFoundException.java
@@ -4,19 +4,7 @@ import br.com.dducl.collective_coffee_marketplace.util.MessageUtil;
 
 public class NotFoundException extends Exception {
 
-    public NotFoundException(Integer id, String entity) {
-        super(String.format("%s com id %d n\u00E3o foi encontrado(a)!", entity, id));
-    }
-
-    public NotFoundException(String identificador, String entity) {
-        super(String.format("%s com identificador %s n\u00E3o foi encontrado(a)!", entity, identificador));
-    }
-
-    public NotFoundException(String code) {
-        super(MessageUtil.getMessage(code));
-    }
-
-    public NotFoundException(String code, String... replace) {
-        super(MessageUtil.getMessage(code, replace));
+    public NotFoundException(String... replace) {
+        super(MessageUtil.getMessage("NAO_ENCONTRADO", replace));
     }
 }
